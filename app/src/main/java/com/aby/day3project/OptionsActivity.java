61a5d64608ea2cd02d263548709513e24646d62f
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-01 20:16:08 -0400

    Added reverse name

--- app/src/main/java/com/aby/day3project/OptionsActivity.java
+++ app/src/main/java/com/aby/day3project/OptionsActivity.java
@@ -13,6 +13,8 @@ public class OptionsActivity extends AppCompatActivity {
     private EditText edtName;
     private Button btnUppercase;
     private Button btnLowercase;
+    private Button btnReverse;
+    private Button btnLength;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,9 @@ public class OptionsActivity extends AppCompatActivity {
 
         btnUppercase = findViewById(R.id.btnUppercase);
         btnLowercase = findViewById(R.id.btnLowercase);
+        btnLength = findViewById(R.id.btnLength);
+        btnReverse = findViewById(R.id.btnReverse);
+
         edtName = findViewById(R.id.edtName);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -50,5 +55,34 @@ public class OptionsActivity extends AppCompatActivity {
             }
         });
 
+        btnLength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int finalString = edtName.getText().toString().length();
+                Intent mIntent = new Intent();
+                mIntent.putExtra("finalString", finalString);
+                setResult(RESULT_OK, mIntent);
+                finish();
+            }
+        });
+        btnReverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String finalString = reverseString(edtName.getText().toString());
+                Intent mIntent = new Intent();
+                mIntent.putExtra("finalString", finalString);
+                setResult(RESULT_OK, mIntent);
+                finish();
+            }
+        });
+
+    }
+
+    private String reverseString(String someText){
+        String output = "";
+        for (int i = someText.length() - 1; i >= 0; i--) {
+            output = output + someText.charAt(i);
+        }
+        return output;
     }
 }
